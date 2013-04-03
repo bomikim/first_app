9147a2a99d5b8a91cf2ff6c381e6b2b527bba9eb
Bomi Kim <bomikim@OpenFactory-iMac-01.local>
2013-04-03 21:24:55 +0900

    starting a new app

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ef53ceff86309cf3da995af89a0c08119c2f798427fe6534ae1056b8f733e8aebb2ee33dbbf6139268f623fe29da83b53f9b2a22ddb44692bbe3cbb650cd849b'
